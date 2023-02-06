439d31f84930c5216259221ae72be4f42a5a024b
Benjamin Okoli <jaminokoli10@gmail.com>
2023-02-06 14:41:00 +0100

    add ECS_role.tf

--- ECS_role.tf
+++ ECS_role.tf
@@ -1,5 +1,5 @@
-resource "aws_iam_role" "metabase_iam_role" {
-  name = "metabase_iam_role"
+resource "aws_iam_role" "ecs_iam_role" {
+  name = "ecs_iam_role"
   assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
 }
 
@@ -14,7 +14,7 @@ data "aws_iam_policy_document" "assume_role_policy" {
   }
 }
 
-resource "aws_iam_role_policy_attachment" "metasebase_Role_policy" {
-  role       = "${aws_iam_role.metabase_iam_role.name}"
+resource "aws_iam_role_policy_attachment" "ecs_Role_policy" {
+  role       = "${aws_iam_role.ecs_iam_role.name}"
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
